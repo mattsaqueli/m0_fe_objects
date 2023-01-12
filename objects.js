36d69679237492a3a44ddf11a505d1b03b41b703
Matt Saqueli <msaqueli22@turing.edu>
2023-01-12 17:45:18 -0600

    Add anwsers to the challenges

--- objects.js
+++ objects.js
@@ -15,22 +15,32 @@ foods = {
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.keys(foods));
+
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
 // YOUR CODE HERE
+
+console.log(Object.values(foods));
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
 // YOUR CODE HERE
 
+console.log(foods.grapes);
+
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
 // YOUR CODE HERE
 
+foods.mangos = 3;
+Object.keys(foods);
+console.log(foods);
 
-#-------------------
+
+//#-------------------
 // Part 2: Email
-#-------------------
+//#-------------------
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -39,21 +49,34 @@ foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email = {
+  sender: "msaqueli@gmail.com",
+  receiver: "sarahbarnard@gmail.com",
+  subject: "Texas trip",
+  files: "3",
+  notes: "Hi"
+}
+
+
 // Write code that logs your email object to the terminal.
 
+console.log(email);
 
 // Write code that logs all of the 'keys' of the email object 
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.keys(email));
+
 // Write code that logs all of the 'values' of the email object 
 // you created above:
 // YOUR CODE HERE
 
+console.log(Object.values(email));
 
-#-------------------
+//#-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+//#-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -99,3 +122,17 @@ console.log(posts[0]);
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+email = [
+  {
+    sender: "msaqueli@gmail.com"
+  },
+  {
+    receiver: "sarahbarnard@gmail.com"
+  },
+  {
+    subject: "Texas trip"
+  }
+];
+
+console.log(email);
